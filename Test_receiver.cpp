87482eb2fee4d98676acafb3c67b4ff3b07b5c34
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-06-20 03:43:26 +0530

    Update Test_receiver.cpp

--- Test_receiver.cpp
+++ Test_receiver.cpp
@@ -17,4 +17,59 @@ TEST_CASE("NULL pointer is passed to read input stream ") {
   REQUIRE(ifTrueReadSuccess == false);
 }
 
+
+TEST_CASE( "Max parameter evaluation"){
   
+  const int NoOfTest2 = 2;
+  float paramMax =0.f;
+  
+  typedef struct inputMatrix3_st {
+                                   float parameterValue;
+                                   float parameterMax;
+  } InputMaxSet;
+
+  InputMaxSet InputSet2[NoOfTest2] = {
+    { chargeRateMax, chargeRateMin + 0.2f },
+    { chargeRateMin, chargeRateMin + 0.2f }
+  };
+
+  float OutputParMax[NoOfTest2] = { chargeRateMax, chargeRateMin + 0.2f };
+
+  for (int TestCaseCounter = 0;TestCaseCounter < NoOfTest2; TestCaseCounter++)
+  {
+    paramMax = InputSet2[TestCaseCounter].parameterMax;
+    //Call function under test 
+    calculateMaxValue(&InputSet2[TestCaseCounter].parameterValue, &paramMax);
+
+    REQUIRE(paramMax == OutputParMax[TestCaseCounter]);
+  }
+}//End Of Max parameter evaluation
+  
+TEST_CASE( "Min parameter evaluation"){
+  
+  const int NoOfTest1 = 2;
+   float paramMin = 0.f;
+  //Input matrix structure
+  typedef struct inputMatrix2_st {
+    float parameterValue;
+    float parameterMin;
+  } InputMinSet;
+
+  //Input output variable data
+  InputMinSet InputSet1[NoOfTest1] = {
+                                     {TempMin, TempMin + 10},
+                                     {TempMax, TempMin+5}
+  };
+
+  float OutputParMin[NoOfTest1] = { TempMin, TempMin + 5 };
+  
+  for (int TestCaseCounter = 0;TestCaseCounter< NoOfTest1; TestCaseCounter++)
+  {
+    paramMin = InputSet1[TestCaseCounter].parameterMin;
+    
+    //Call function under test 
+    calculateMinValue(&InputSet1[TestCaseCounter].parameterValue, &paramMin);
+
+    REQUIRE(paramMin == OutputParMin[TestCaseCounter]);
+  }
+}//End Of Min parameter evaluation
