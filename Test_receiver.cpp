8c969912a68145596c87f5dc65fedb1a8dc62a58
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-06-20 03:16:47 +0530

    Update Test_receiver.cpp

--- Test_receiver.cpp
+++ Test_receiver.cpp
@@ -45,3 +45,91 @@ TEST_CASE("Test if data was extracted correctly")
   REQUIRE(paramBuffer[Temperature] == 98);
   REQUIRE((paramBuffer[chargeRate] - 0.1) < infinitesimally_small);
 }
+
+TEST_CASE( " Simple moving average calculation ")
+{
+  const int NoOfTest = 2;
+  
+  //Input data matrix structure for the test
+  typedef struct inputMatrix1_st {
+    float InputmovingAvg;
+    float previousIndividualParValueSet[movingAvgDataCount];
+  } simpleAvg;
+  
+  //Input and output variables
+  simpleAvg InputsimpleAvg[NoOfTest] = {
+                               {30, {0,0,0,0,0} }, //Random input set 
+                               {0,  {12,13,14,15,16}}, //Random input set  
+
+  };
+
+  float OutputmovingAvg[NoOfTest] = { 0.f, 14.f };
+  
+  for (int  TestCaseCounter = 0 ;TestCaseCounter <  NoOfTest; TestCaseCounter++)
+  { 
+   float movingAvg = InputsimpleAvg[TestCaseCounter].InputmovingAvg;
+   //Call function under test 
+   simpleMovingAvg(InputsimpleAvg[TestCaseCounter].previousIndividualParValueSet, &movingAvg);
+   //Expected result
+   REQUIRE((movingAvg - OutputmovingAvg[TestCaseCounter])< infinitesimally_small);
+  }
+  
+}
+
+TEST_CASE( "Min parameter evaluation"){
+  
+  const int NoOfTest1 = 2;
+   float paramMin = 0.f;
+  //Input matrix structure
+  typedef struct inputMatrix2_st {
+    float parameterValue;
+    float parameterMin;
+  } InputMinSet;
+
+  //Input output variable data
+  InputMinSet InputSet1[NoOfTest1] = {
+                                     {TempMin, TempMin + 10},
+                                     {TempMax, TempMin+5}
+  };
+
+  float OutputParMin[NoOfTest1] = { TempMin, TempMin + 5 };
+  
+  for (int TestCaseCounter = 0;TestCaseCounter< NoOfTest1; TestCaseCounter++)
+  {
+    paramMin = InputSet1[TestCaseCounter].parameterMin;
+    
+    //Call function under test 
+    calculateMinValue(&InputSet1[TestCaseCounter].parameterValue, &paramMin);
+
+    REQUIRE(paramMin == OutputParMin[TestCaseCounter]);
+  }
+}
+
+TEST_CASE( "Min parameter evaluation"){
+  
+  const int NoOfTest2 = 2;
+  float paramMax =0.f;
+  
+  typedef struct inputMatrix3_st {
+                                   float parameterValue;
+                                   float parameterMax;
+  } InputMaxSet;
+
+  InputMaxSet InputSet2[NoOfTest2] = {
+    { chargeRateMax, chargeRateMin + 0.2f },
+    { chargeRateMin, chargeRateMin + 0.2f }
+  };
+
+  float OutputParMax[NoOfTest2] = { chargeRateMax, chargeRateMin + 0.2f };
+
+  for (int TestCaseCounter = 0;TestCaseCounter < NoOfTest2; TestCaseCounter++)
+  {
+    paramMax = InputSet2[TestCaseCounter].parameterMax;
+    //Call function under test 
+    calculateMaxValue(&InputSet2[TestCaseCounter].parameterValue, &paramMax);
+
+    EXPECT(paramMax == OutputParMax[TestCaseCounter]);
+  }
+  
+}
+  
