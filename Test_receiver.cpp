1154d707a2dd48448a9c7fa4463251b331c03e24
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-06-20 03:45:18 +0530

    Update Test_receiver.cpp

--- Test_receiver.cpp
+++ Test_receiver.cpp
@@ -73,3 +73,17 @@ TEST_CASE( "Min parameter evaluation"){
     REQUIRE(paramMin == OutputParMin[TestCaseCounter]);
   }
 }//End Of Min parameter evaluation
+
+TEST_CASE("Test if data was extracted correctly")
+{
+  //Initialize buffer
+  float paramBuffer[NoOfPar] = { 0 };
+  char paramSet1[] = "98;0.1;\n"; //Input stream
+
+  //Call function under test
+  extractParametersFromInputSet(paramSet1, paramBuffer);
+  
+  //Expected reults
+  REQUIRE(paramBuffer[Temperature] == 98);
+  REQUIRE((paramBuffer[chargeRate] - 0.1) < infinitesimally_small);
+}
