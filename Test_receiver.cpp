f34da1d7ebff3b25943abb93cb6a78e8f0f82397
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-06-20 02:52:32 +0530

    Update Test_receiver.cpp

--- Test_receiver.cpp
+++ Test_receiver.cpp
@@ -1,4 +1,4 @@
-
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "Receiver\Receiver.c"
 #include "Receiver\Type_def.h"
 #include <stdio.h>
