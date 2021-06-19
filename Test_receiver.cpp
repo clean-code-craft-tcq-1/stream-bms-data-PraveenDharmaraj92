55bfae4267e86a37c5a2a03620be016dc153f99f
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-06-20 03:05:59 +0530

    Added more tests

--- Test_receiver.cpp
+++ Test_receiver.cpp
@@ -10,10 +10,38 @@
 
 //NULL pointer is passed to read input parameter stream 
 TEST_CASE("NULL pointer is passed to read input stream ") {
-
   // Call function under test
   bool ifTrueReadSuccess = readInputFromConsole(NULL);
 
   //Expected result
   REQUIRE(ifTrueReadSuccess == false);
+}
+
+//Input stream of data was read   
+TEST_CASE("Input stream of data was read") {
+  //eg. of expected input as per sender designed
+  //par1;par2;\n
+  //10;0.1;\n
+  printf("Enter a parameter set in json format: \n ");
+  char paramSet[lengthOfParamset];
+
+  // Call function under test
+  ifTrueReadSuccess = readInputFromConsole(paramSet);
+  
+  //Expected result
+  REQUIRE(ifTrueReadSuccess == true);
+}
+
+TEST_CASE("Test if data was extracted correctly")
+{
+  //Initialize buffer
+  float paramBuffer[NoOfPar] = { 0 };
+  char paramSet1[] = "98;0.1;\n"; //Input stream
+
+  //Call function under test
+  extractParametersFromInputSet(paramSet1, paramBuffer);
+  
+  //Expected reults
+  REQUIRE(paramBuffer[Temperature] == 98);
+  REQUIRE((paramBuffer[chargeRate] - 0.1) < infinitesimally_small);
 }
