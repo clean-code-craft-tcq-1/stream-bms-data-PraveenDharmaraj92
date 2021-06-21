a4f73faab40e9ebc6e435a72c415496dfb84ac8e
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-06-21 15:37:17 +0530

    Update Receiver.h

--- Receiver/Receiver.h
+++ Receiver/Receiver.h
@@ -8,3 +8,7 @@ void delay(int number_of_seconds);
 void calculateMaxValue(float *individualParameter, float *individualParamMax);
 void calculateMinValue(float *individualParameter, float *individualParamMin);
 void simpleMovingAvg(float *previousIndividualParValueSet, float movingAvg);
+void reportfailure(int readFailureCounter);
+#if (UNITTEST == 1)
+void scanf_mock(const char *format,char *ptr);
+#endif
